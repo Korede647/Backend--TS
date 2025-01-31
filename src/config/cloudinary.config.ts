576b270e6371bd923a0216654976f12280265ada
Korede Anunusor <koredeanunusor@gmai.com>
2025-01-31 20:47:35 +0100

    Cloudinary modified

--- src/config/cloudinary.config.ts
+++ src/config/cloudinary.config.ts
@@ -9,8 +9,8 @@ dotenv.config();
 // Configure Cloudinary
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
+  api_key: process.env.CLOUDINARY_CLOUD_API_KEY,
+  api_secret: process.env.CLOUDINARY_CLOUD_API_SECRET,
 });
 
 // Configure Cloudinary Storage for Profile Images
